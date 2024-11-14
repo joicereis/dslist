ce49e1ff19269b0068e94a84c4d470e1b20b18e4
joice.jdosreis@gmail.com <joice.jdosreis@gmail.com>
2024-11-13 23:02:55 -0300

    Criação de DTO com os atributos básicos do Game

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMininoDTO.java
@@ -0,0 +1,45 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameMininoDTO {
+	private Long id;
+	private String title;
+	private Integer year; 
+	private String imgUrl;
+	private String shortDescription;
+	
+	public GameMininoDTO() {
+		
+	}
+
+	public GameMininoDTO(Game entity) {
+		id = entity.getId(); //busca o id da entitidade e salva no id do DTO
+		title = entity.getTitle();
+		year = entity.getYear();
+		imgUrl = entity.getImgUrl();
+		shortDescription = entity.getShortDescription();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	
+	
+}
