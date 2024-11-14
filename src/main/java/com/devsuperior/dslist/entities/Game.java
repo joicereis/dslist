31980ace44e70d8b4cb4ef2bcf8709ad7450d54e
joice.jdosreis@gmail.com <joice.jdosreis@gmail.com>
2024-11-13 22:46:45 -0300

    Adicionados jogos na tabela tb_game

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -24,7 +24,11 @@ public class Game {
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT") //informa ao sql que o campo deve permitir mais de 255 caracteres
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT") //informa ao sql que o campo deve permitir mais de 255 caracteres
 	private String longDescription;
 	
 	public Game() {	
